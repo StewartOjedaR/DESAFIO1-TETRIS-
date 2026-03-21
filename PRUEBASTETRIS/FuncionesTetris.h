94b27f75b1b6aef7698c30fc5d8acb065b24bdde
StewartOjedaR <stewart.ojeda@udea.edu.co>
2026-03-20 21:31:35 -0500

    algunas lineas arregladas

--- PRUEBASTETRIS/FuncionesTetris.h
+++ PRUEBASTETRIS/FuncionesTetris.h
@@ -2,6 +2,7 @@
 #define FUNCIONESTETRIS_H
 #include<iostream>
 using namespace std;
+/*Crea un arreglo de char (8bits) dinamico en el heap deltamaño que ingrese el usuario*/
 unsigned char *CrearTablero(int Ancho,  int  Alto){
 int valores=(Ancho*Alto)/(sizeof(unsigned char)*8);//Mi arreglo es de tipo unsigned char (8bits), y necesito saber cuanto bits tendra mi tablero, aqui se cuantos 'char' de 8 bits necesto para completar mi tablero
 unsigned char* Tablero=new unsigned char[valores]; //Un arreglo dinamico pues el tablero varia dependiendo del jugador
@@ -19,6 +20,7 @@ for (int i =0 ; i<valores ; i++){//recorro cada elemento de mi arreglo
 }
 return Tablero;
 }
+/*Muestra en pantalla los bits que contenga en arreglo anteriormente creado*/
 
 void MostrarTablero(int Ancho, int Alto, unsigned char *tablero,short *Puntuacion){//pido alto, ancho y la direccion del arregli
 int contador=0;//contador
@@ -50,6 +52,8 @@ return Tetros[numero];
 }
 
 
+/*Rota la figura en el un arreglo anteriormente creado, usando punteros puedo cambiar las figuras
+previamente creadas con un tipo de dato de 16bits, en este caso, unsigned short(unsigned para utilizar todos los bits)*/
 
 void RotarFigura(unsigned short posicion ,unsigned short *pFiguras, short *Grados){
 //0x0f00,0x0660,0x0072,0x0036,0x0063,0x0226,0x0223,
@@ -233,7 +237,7 @@ for (int i=(sizeof(a)*8)-1;i>=0;i--){
 }
 
 
-
+/*Imprime una figura 4*4, originalmente se creo con el animo de para mostras la siguiente figura*/
 void ImpFig(unsigned short *pFiguras){
 int conta=0;
 for (int i=15;i>=0;i--){
@@ -245,7 +249,9 @@ if (conta==4){cout<<endl;conta=0;}
 }
 }
 
-
+/* Mascara de bits hecha de arreglos como el tablero original salvo que esto es para aplicarle operadores
+a nivel bit con el animo de manipular la el juego, tiene diferencias a la hora de respuesta,
+mientras una me crea un tablero con sus 'paredes',esta me crea un arreglo completamente en blanco*/
 unsigned char *MascaraTablero(int Ancho,  int  Alto){
 int valores=(Ancho*Alto)/(sizeof(unsigned char)*8);//Mi arreglo es de tipo unsigned char (8bits), y necesito saber cuanto bits tendra mi tablero, aqui se cuantos 'char' de 8 bits necesto para completar mi tablero
 unsigned char* Tablero=new unsigned char[valores]; //Un arreglo dinamico pues el tablero varia dependiendo del jugador
@@ -265,7 +271,7 @@ return Tablero;
 }
 
 
-
+/*Inserta la figura en el tablero*/
 void InsertFig(unsigned short*pFigura,int Alto, int Ancho, int PosInicial,unsigned char*pTablero,unsigned char*pMaskTablero){
 int BitFigura=(sizeof(unsigned short)*8)-1;
 int cont=0;
@@ -285,8 +291,10 @@ if (bit>1+(PosInicial+(4*Ancho))){break;}
 
 }
 
-
-
+/*A falta de tiempo cree "funciones de control rapido", para manipular el tablero y 
+las fichas, son pequeñas pero cumplen su pequeña funcion*/
+/*Borra la figura para dejar paso a la siguiente, algo como actualizador de fotogramas que baja el indice de la 
+posicion original */
 void BorrarBajarFig(unsigned short*pFigura,int Alto, int Ancho,
 unsigned char*pTablero,unsigned char*MaskTablero,int *pPosInicial){
 
@@ -297,7 +305,7 @@ for (int i=0;i<(Ancho*Alto)/(sizeof(unsigned char)*8);i++){
 MaskTablero[i]=MaskTablero[i]^MaskTablero[i];
 }*pPosInicial+=Ancho;
 }
-
+/*Borra y desplaza la posicion original para moverla a la izquierda*/
 void BorrarMovIzqFig(unsigned short*pFigura,int Alto, int Ancho,
 unsigned char*pTablero,unsigned char*MaskTablero,int *pPosInicial){
 
@@ -308,6 +316,8 @@ for (int i=0;i<(Ancho*Alto)/(sizeof(unsigned char)*8);i++){
 MaskTablero[i]=MaskTablero[i]^MaskTablero[i];
 }*pPosInicial-=1;
 }
+
+/*Borra y deplaza a la derecha*/
 void BorrarMovDerFig(unsigned short*pFigura,int Alto, int Ancho,
 unsigned char*pTablero,unsigned char*MaskTablero,int *pPosInicial){
 
@@ -318,7 +328,8 @@ for (int i=0;i<(Ancho*Alto)/(sizeof(unsigned char)*8);i++){
 MaskTablero[i]=MaskTablero[i]^MaskTablero[i];
 }*pPosInicial+=1;
 }
-
+/*borra la figura del rablero y de la mascara,util para actualizar
+*/
 void BorrarFig(unsigned short*pFigura,int Alto, int Ancho,
 unsigned char*pTablero,unsigned char*MaskTablero){
 
@@ -329,7 +340,7 @@ for (int i=0;i<(Ancho*Alto)/(sizeof(unsigned char)*8);i++){
 MaskTablero[i]=MaskTablero[i]^MaskTablero[i];
 }
 }
-
+/*inserta figura en la mascara para despues verificar colisiones*/
 void InsertFigMaskIzq(unsigned short*pFigura,int Alto, int Ancho, int PosInicial,unsigned char*pMaskTablero){
 int BitFigura=(sizeof(unsigned short)*8)-1;
 PosInicial-=1;
@@ -348,7 +359,7 @@ if (bit>1+(PosInicial+(4*Ancho))){break;}
 }
 
 }
-
+/*inserta figura en la mascara para despues verificar colisiones*/
 void InsertFigMaskDer(unsigned short*pFigura,int Alto, int Ancho, int PosInicial,unsigned char*pMaskTablero){
 int BitFigura=(sizeof(unsigned short)*8)-1;
 PosInicial+=1;
@@ -369,7 +380,7 @@ if (bit>1+(PosInicial+(4*Ancho))){break;}
 }
 
 
-
+/*Se utiliza para colisiones(funcional pero no se usa), preferi otra funcion que verifique bit a bit las colisiones*/
 
 bool ColisionLateralIzq(int Alto, int Ancho,unsigned char* pTablero, unsigned char* pMaskTablero, bool* pColision) {
 *pColision = false;
@@ -407,7 +418,7 @@ if (*pColision==true){break;}
    return *pColision;
 }
 
-
+/*Originalmente para recuperar una mascara previamente borrada*/
 
 void RecuperarMaskFig(unsigned short*pFigura,int Alto, int Ancho, int PosInicial,unsigned char*pMaskTablero){
 int BitFigura=(sizeof(unsigned short)*8)-1;
@@ -427,9 +438,10 @@ if (bit>1+(PosInicial+(4*Ancho))){break;}
 
 }
 
-
-void InsertFigMaskAbajo(unsigned short*pFigura,int Alto, int Ancho, int PosInicial,unsigned char*pMaskTablero){
+/*inseta la figura de la mascara abajo para checar colisiones*/
+void InsertFigMaskAbajo(unsigned short*pFigura,int Alto, int Ancho, int PosInicial,unsigned char*pMaskTablero,bool*pFijar, bool*pColision){
 int BitFigura=(sizeof(unsigned short)*8)-1;
+if (PosInicial+Ancho>Alto*Ancho){*pFijar=true; *pColision=true; return;}
 PosInicial+=Ancho;
 int cont=0;
 for (int bit=0; bit<Alto*Ancho;bit++){
@@ -444,8 +456,9 @@ for (int bit=0; bit<Alto*Ancho;bit++){
     }
 if (bit>1+(PosInicial+(4*Ancho))){break;}
 }
-
 }
+/*Borra la mascara y lo deja en blanco*/
+
 void BorrarMaskFig(unsigned short*pFigura,int Alto, int Ancho,
 unsigned char*MaskTablero){
 for (int i=0;i<(Ancho*Alto)/(sizeof(unsigned char)*8);i++){
@@ -453,7 +466,7 @@ MaskTablero[i]=MaskTablero[i]^MaskTablero[i];
 }
 }
 
-
+/*Funcion que no se usara aunque funciona */
 bool ColisionLateralDer(int Alto, int Ancho,unsigned char* pTablero, unsigned char* pMaskTablero, bool* pColision) {
 *pColision = false;
 char ContadorBits=0;
@@ -490,11 +503,11 @@ if (*pColision==true){break;}
    return *pColision;
 }
 
-
+/*Compara bit a bit el arreglo , si algun bit coincide ¡Colision!*/
 bool Colision(int Alto, int Ancho,unsigned char* pTablero, unsigned char* pMaskTablero, bool* pColision){
 
 bool MaskBitFig; bool MaskBitTab;
-        for (int i=0; i<72;i++){
+        for (int i=0; i<Alto*Ancho;i++){
              for (int j=0;j<(Ancho*Alto)/8;j++){
                   for (int bit=0;bit<=7;bit++){
                    MaskBitFig=pMaskTablero[j]&1<<bit;
@@ -508,6 +521,9 @@ cout<<"\tTETRIS by Stewart Ojeda\t\nPara iniciar ingresa el tamano del tablero \
 cout<<"\nEl tamano del tablero es 8:1 (1Unidad=8Bloques)";
 cout<<"\nIngrese la altura: ";
 }
+
+
+
 
 #endif // FUNCIONESTETRIS_H
 
--- PRUEBASTETRIS/main.cpp
+++ PRUEBASTETRIS/main.cpp
@@ -7,94 +7,99 @@ bool*pColision=&colision;bool Fijar=false;bool*pFijar=&Fijar;
 int PosInicial=5; int *pPosInicial=&PosInicial; short *pGrados=&Grado; short*pPuntuacion=&Puntuacion;
 unsigned short PosicionFicha=0; unsigned short Figura=Figuras(PosicionFicha);unsigned short *pFigura=&Figura; unsigned short *pPosicionFicha=&Figura;
 int main(){
-   Mensaje();
-   cin>>Alto;
-   cout<<"Ingrese el ancho: ";cin>>Ancho;
-   unsigned char *pTablero=CrearTablero(Ancho,Alto);
-   unsigned char *pMaskTablero=MascaraTablero(Ancho,Alto);
-   MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
-   cout<<"\nPRESIONE CUALQUIER TECLA PARA INICIAR";
-   bool SalirDelJuego;
-   cin>>Entrada;
-   SalirDelJuego=true;
-   InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-do
-{
-   MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
-   cin>>Entrada;
-   switch (Entrada){
-    
-case 'A'|'a':
-       BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
-       InsertFigMaskIzq(pFigura,Alto,Ancho,PosInicial,pMaskTablero);
-       cout<<ColisionLateralIzq(Alto,Ancho,pTablero,pMaskTablero,pColision);cout<<endl;
-        if (ColisionLateralIzq(Alto,Ancho,pTablero,pMaskTablero,pColision)){
-           BorrarMaskFig(pFigura,Ancho,Alto,pMaskTablero);
-           InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);*pColision=false;
-           break;
-        }
-        BorrarMaskFig(pFigura,Ancho,Alto,pMaskTablero);
-        InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-        BorrarMovIzqFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
-        InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-        break;
-
-    case 's'|'S':
+    Mensaje();
+    cin>>Alto;
+    cout<<"Ingrese el ancho: ";cin>>Ancho;
+    unsigned char *pTablero=CrearTablero(Ancho,Alto);
+    unsigned char *pTableroPermanente=CrearTablero(Ancho,Alto);
+    unsigned char *pMaskTablero=MascaraTablero(Ancho,Alto);
+    MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
+    cout<<"\nPRESIONE CUALQUIER TECLA PARA INICIAR";
+    bool SalirDelJuego;
+    cin>>Entrada;
+    SalirDelJuego=true;
+    InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+    do
+    {
         MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
-        BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
-MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
-        InsertFigMaskAbajo(pFigura,Alto,Ancho,PosInicial,pMaskTablero,pFijar,pColision,pTablero);
-           if (*pFijar==true){
-            pColision=false;BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+        cin>>Entrada;
+        switch (Entrada){
+
+        case 'a':
+            BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+            InsertFigMaskIzq(pFigura,Alto,Ancho,PosInicial,pMaskTablero);
+            cout<<ColisionLateralIzq(Alto,Ancho,pTablero,pMaskTablero,pColision);cout<<endl;
+            if (ColisionLateralIzq(Alto,Ancho,pTablero,pMaskTablero,pColision)){
+                BorrarMaskFig(pFigura,Ancho,Alto,pMaskTablero);
+                InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);*pColision=false;
+                break;
+            }
+            BorrarMaskFig(pFigura,Ancho,Alto,pMaskTablero);
             InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-            *pPosInicial=5;*pFigura=+1;if(*pFigura>8){*pFigura=0;}
-            
+            BorrarMovIzqFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
+            InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+            break;
+
+        case 's':
+            if (PosInicial+Ancho>(Ancho*Alto)-(Ancho*4)){
+                BorrarMaskFig(pFigura,Ancho,Alto,pMaskTablero);
+                InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+                *pColision=0;
+                *pColision=0;BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+                InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+                *pPosInicial=5;*pFigura=+1;
+                if(*pFigura>8){*pFigura=0;}
+                break;
+            }
+            MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
+            BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+            InsertFigMaskAbajo(pFigura,Alto,Ancho,PosInicial,pMaskTablero,pFijar,pColision);
+            if (*pFijar==true){
+                *pColision=false;BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+                InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+                *pPosInicial=5;*pFigura=+1;if(*pFigura>8){*pFigura=0;}
+
             }
 
+            BorrarMaskFig(pFigura,Alto,Ancho,pMaskTablero);
+            BorrarBajarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
 
+            InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+            break;
 
-MostrarTablero(Ancho,Alto,pTablero,pPuntuacion);
-cout<<Colision(Alto,Ancho,pTablero,pMaskTablero,pColision);
-        if (Colision(Alto,Ancho,pTablero,pMaskTablero,pColision))
-        BorrarMaskFig(pFigura,Alto,Ancho,pMaskTablero);
-        BorrarBajarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
-        
-        InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-        break;
-    
-    case 'W'|'w':
-        BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
-        RotarFigura(PosicionFicha,pFigura,pGrados);
-        InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-         break;
+        case 'w':
+            BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+            RotarFigura(PosicionFicha,pFigura,pGrados);
+            InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+            break;
 
-    case 'D'|'d':
-        BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
-        InsertFigMaskDer(pFigura,Alto,Ancho,PosInicial,pMaskTablero);
-           if (  Colision(Alto,Ancho,pTablero,pMaskTablero,pColision)){
+        case 'd':
+            BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+            InsertFigMaskDer(pFigura,Alto,Ancho,PosInicial,pMaskTablero);
+            if (  Colision(Alto,Ancho,pTablero,pMaskTablero,pColision)){
                 BorrarMaskFig(pFigura,Ancho,Alto,pMaskTablero);
                 InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
                 *pColision=false;
+                break;
+            }
+            BorrarMovDerFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
+            InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
+            break;
+
+        case 'Q'|'q': SalirDelJuego=false;
+            break;
+
+        default:
+            BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
+            InsertFigMaskAbajo(pFigura,Alto,Ancho,PosInicial,pMaskTablero,pFijar,pColision);
+            if (Colision(Alto,Ancho,pTablero,pMaskTablero,pColision)){*pColision=0;
+            }
+            BorrarBajarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
+
+            InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
             break;
         }
-        BorrarMovDerFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
-        InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-        break;
-
-    case 'Q'|'q': SalirDelJuego=false;
-        break;
-   
-    default:
-        BorrarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero);
-        InsertFigMaskAbajo(pFigura,Alto,Ancho,PosInicial,pMaskTablero,pFijar,pColision,pTablero);
-        if (Colision(Alto,Ancho,pTablero,pMaskTablero,pColision)){;pColision=false;
-}
-        BorrarBajarFig(pFigura,Alto,Ancho,pTablero,pMaskTablero,pPosInicial);
-        
-        InsertFig(pFigura,Alto,Ancho,PosInicial,pTablero,pMaskTablero);
-        break;
-    }
-}while (SalirDelJuego==true);
-delete[] pTablero,pMaskTablero;//libero memoria
-return 0;
+    }while (SalirDelJuego==true);
+    delete[] pTablero,pMaskTablero;//libero memoria
+    return 0;
 }
